e2348f41061b3f596257a3b589fe380ea272cb01
Martin Fernandez <Martin.Fernandez@endava.net>
2021-03-16 17:16:42 -0300

    Branch init 1.0

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("APPLICATION STARTED !!!!!!!");
+		System.out.println("APPLICATION STARTED RELEASE1.0!!!!!!!");
 	}
 
 }
